62e78e0627bc263004e14008ce73c3150388a5ef
Andrea Salamone <andrea.sala96@gmail.com>
2020-09-01 18:45:14 +0200

    core: build with tsup

--- src/local.ts
+++ src/local.ts
@@ -2,9 +2,9 @@ import {generator} from './generator';
 
 const storage: Storage = typeof window !== 'undefined' ? window.localStorage : undefined;
 
-export const {
-    readable,
-    writable,
-    derived,
-    get
-} = generator(storage);
+const g = generator(storage);
+
+export const readable = g.readable;
+export const writable = g.writable;
+export const derived = g.derived;
+export const get = g.get;
