1f8f8d747051a04163265991a76bc32006610c28
SillyFreak <sillyfreak.ck@gmail.com>
2021-01-04 17:14:50 +0100

    Partially revert "chore: update dependencies"
    This partially reverts commit 3f21c3b4bbc529b9fc10886c08153bde6935ae9a, fixing #9 (SSR regression).

--- src/local.ts
+++ src/local.ts
@@ -1,6 +1,8 @@
 import {generator} from './generator';
 
-const g = generator(window.localStorage);
+const storage: Storage = typeof window !== 'undefined' ? window.localStorage : undefined;
+
+const g = generator(storage);
 
 export const readable = g.readable;
 export const writable = g.writable;
